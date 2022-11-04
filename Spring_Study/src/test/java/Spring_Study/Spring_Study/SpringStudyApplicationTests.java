d31972ffcf94fa265d253f50a7b3f5e9b1bdf5c6
Jade_Rain <83251008+jaderain765@users.noreply.github.com>
2022-11-04 17:10:11 +0900

    프로젝트 업데이트

---
+++ Spring_Study/src/test/java/Spring_Study/Spring_Study/SpringStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package Spring_Study.Spring_Study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
